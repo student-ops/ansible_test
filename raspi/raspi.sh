29767eae97781d11303534349c75e4f9666b4045
stundet-ops <gogakuec@mail.com>
2023-07-26 15:25:46 +0900

    raspi still not work

--- raspi/raspi.sh
+++ raspi/raspi.sh
@@ -1,4 +1,4 @@
 wget https://downloads.raspberrypi.org/raspios_arm64/root.tar.xz
 
 docker image import root.tar.xz lakky/raspios_lite
-docker run -it --rm lakky/raspios_lite /bin/bash
+docker run  -d lakky/raspios_lite /bin/bash sleep infinity
