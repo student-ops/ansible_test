90bcb1268baadc2c7de0c9628789c39d4b633e7f
stundet-ops <gogakuec@mail.com>
2023-07-27 14:46:56 +0900

    update docker setup

--- raspi/raspi.sh
+++ raspi/raspi.sh
@@ -12,3 +12,14 @@ docker image import root.tar lakky/raspios_lite:latest
 # Run the container with a specific name
 docker run -d -p 2222:22 --name raspiosc lakky/raspios_lite:latest /bin/bash -c "ls; sleep infinity"
 
+
+#!/bin/bash
+
+# Define the inventory file path
+INVENTORY_PATH=./inventory.ini
+
+# Define the playbook file path
+PLAYBOOK_PATH=./install_docker.yml
+
+# Run the Ansible playbook
+ansible-playbook -i $INVENTORY_PATH $PLAYBOOK_PATH
